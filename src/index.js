26c80eabcc0eaa3971136743edfde784eea57450
lgramirez <gramirez.developer@gmail.com>
2021-01-04 10:44:12 -0400

    Create a component

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import GifExpertApp from "./GifExpertApp";
+
+ReactDOM.render(<GifExpertApp />, document.getElementById("root"));
