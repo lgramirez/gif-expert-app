31923787e0a9c14c044c568773b0c671306c86a5
lgramirez <gramirez.developer@gmail.com>
2021-01-04 12:38:26 -0400

    Create component and add eslint to gitignore

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,28 +1,26 @@
 import React, { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
 
 const GifExpertApp = () => {
-  // const categories = ["One Punch", "Samurai X", "Dragon Ball"];
-
   const [categories, setCategories] = useState([
     "One Punch",
     "Samurai X",
     "Dragon Ball",
   ]);
 
-  const handleAdd = () => {
-    setCategories([...categories, "Good Doctor"]);
-    // o podemos usar la otra forma de modificar el estado
-    // donde recibimos como primer param el estado anterior y
-    // devolvemos el nuevo estado
-    // setCategories( cats => [...cats, 'Good Doctor'])
-  };
+  // const handleAdd = () => {
+  //   setCategories([...categories, "Good Doctor"]);
+  //   // o podemos usar la otra forma de modificar el estado
+  //   // donde recibimos como primer param el estado anterior y
+  //   // devolvemos el nuevo estado
+  //   // setCategories( cats => [...cats, 'Good Doctor'])
+  // };
 
   return (
     <>
       <h2>GifExpertApp</h2>
+      <AddCategory />
       <hr />
-
-      <button onClick={handleAdd}>Agregar</button>
 
       <ol>
         {categories.map((category) => {
