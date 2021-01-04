75e9ec0f1778cff76b1d2e6773c26f3a170f616d
lgramirez <gramirez.developer@gmail.com>
2021-01-04 12:13:34 -0400

    Create a category list

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 const GifExpertApp = () => {
+  // const categories = ["One Punch", "Samurai X", "Dragon Ball"];
+
+  const [categories, setCategories] = useState([
+    "One Punch",
+    "Samurai X",
+    "Dragon Ball",
+  ]);
+
+  const handleAdd = () => {
+    setCategories([...categories, "Good Doctor"]);
+    // o podemos usar la otra forma de modificar el estado
+    // donde recibimos como primer param el estado anterior y
+    // devolvemos el nuevo estado
+    // setCategories( cats => [...cats, 'Good Doctor'])
+  };
+
   return (
     <>
       <h2>GifExpertApp</h2>
       <hr />
+
+      <button onClick={handleAdd}>Agregar</button>
+
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
     </>
   );
 };
