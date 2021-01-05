c9cf752679b18d8432d92884641026d9d1efe2bb
lgramirez <gramirez.developer@gmail.com>
2021-01-05 18:33:52 -0400

    Fetch API get desired images

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,12 +1,9 @@
 import React, { useState } from "react";
 import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
 
 const GifExpertApp = () => {
-  const [categories, setCategories] = useState([
-    "One Punch",
-    "Samurai X",
-    "Dragon Ball",
-  ]);
+  const [categories, setCategories] = useState(["One Punch"]);
 
   // const handleAdd = () => {
   //   setCategories([...categories, "Good Doctor"]);
@@ -23,9 +20,9 @@ const GifExpertApp = () => {
       <hr />
 
       <ol>
-        {categories.map((category) => {
-          return <li key={category}>{category}</li>;
-        })}
+        {categories.map((category) => (
+          <GifGrid key={category} category={category} />
+        ))}
       </ol>
     </>
   );
---
+++ src/components/GifGrid.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export const GifGrid = ({ category }) => {
+  const getGifs = async () => {
+    const url =
+      "https://api.giphy.com/v1/gifs/search?q=rick+and+morthy&limit=10&api_key=G3x1V4VNTyP4snSskhcKfu11XFgJbUSw";
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+
+    const gifs = data.map((img) => {
+      return {
+        id: img.id,
+        title: img.title,
+        url: img.images.downsized_medium.url,
+      };
+    });
+
+    console.log(gifs);
+  };
+
+  getGifs();
+
+  return (
+    <>
+      <h3>{category}</h3>
+    </>
+  );
+};
