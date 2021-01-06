c598f577509aca3dcea613f2da771ad915c4343b
lgramirez <gramirez.developer@gmail.com>
2021-01-06 17:49:17 -0400

    Add animations with animate.css

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -7,8 +7,10 @@ export const GifGrid = ({ category }) => {
 
   return (
     <>
-      <h3>{category}</h3>
-      {loading && <p>Loading...</p>}
+      <h3 className="animate__animated animate__fadeInLeft">{category}</h3>
+      {loading && (
+        <p className="animate__animated animate__flash">Loading...</p>
+      )}
       <div className="card-grid">
         {images.map((image) => {
           return <GifGridItem key={image.id} {...image} />;
