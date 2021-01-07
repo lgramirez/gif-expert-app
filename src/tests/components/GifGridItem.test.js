d071798734abdf9239ba3ea1b14998ad3700af9a
lgramirez <gramirez.developer@gmail.com>
2021-01-07 10:19:21 -0400

    Add proptypes and update snapshot

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -4,8 +4,11 @@ import { GifGridItem } from "../../components/GifGridItem";
 import "../../setupTests";
 
 describe("Pruebas para GifGridItem", () => {
+  const title = "titulo";
+  const url = "https://localhost/algo.jpg";
+
   test("debe mostar el componente correctamente (hacer match con un snapshot)", () => {
-    const wrapper = shallow(<GifGridItem />);
+    const wrapper = shallow(<GifGridItem title={title} url={url} />);
     expect(wrapper).toMatchSnapshot();
   });
 });
