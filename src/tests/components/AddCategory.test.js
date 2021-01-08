a37778aae54b613eb79bc39fd2e5f41523c7232a
lgramirez <gramirez.developer@gmail.com>
2021-01-08 11:02:57 -0400

    Simulate changes on input

--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -4,9 +4,21 @@ import { AddCategory } from "../../components/AddCategory";
 
 describe("Pruebas para <AddCategory />", () => {
   const setCategories = () => {};
+  const wrapper = shallow(<AddCategory setCategories={setCategories} />);
 
   test("debe de mostrarse correctamente", () => {
-    const wrapper = shallow(<AddCategory setCategories={setCategories} />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  test("debe de cambiar la caja de texto", () => {
+    const input = wrapper.find("input");
+    const value = "Hola Mundo";
+
+    input.simulate("change", { target: { value } });
+
+    // para hacer este expect tuvimos que añadir un parrafo <p></p>
+    // al componente, actualizar el snapshot y comprobar que el
+    // parrafo se actualice con el valor del valor que cambiamos en el input
+    expect(wrapper.find("p").text().trim()).toBe(value);
   });
 });
