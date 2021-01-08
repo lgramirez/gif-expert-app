c4b2094769ad27e4cb5d2853380181dc474fa3f9
lgramirez <gramirez.developer@gmail.com>
2021-01-08 10:49:59 -0400

    First test for <AddCateroy />

---
+++ src/tests/components/AddCategory.test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { AddCategory } from "../../components/AddCategory";
+
+describe("Pruebas para <AddCategory />", () => {
+  const setCategories = () => {};
+
+  test("debe de mostrarse correctamente", () => {
+    const wrapper = shallow(<AddCategory setCategories={setCategories} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
