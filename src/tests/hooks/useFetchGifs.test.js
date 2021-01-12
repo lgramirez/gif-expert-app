9d572af86458487f2f8b592938af40db31b59995
lgramirez <gramirez.developer@gmail.com>
2021-01-12 17:07:48 -0400

    Test waitForNextUpdate

--- src/tests/hooks/useFetchGifs.test.js
+++ src/tests/hooks/useFetchGifs.test.js
@@ -2,10 +2,19 @@ import { renderHook } from "@testing-library/react-hooks";
 import { useFetchGifs } from "../../hooks/useFetchGifs";
 
 describe("Pruebas en el hook useFetchGifs", () => {
-  test("debe de retornar el estado inicial", () => {
-    const { result } = renderHook(() => useFetchGifs("bbt"));
+  test("debe de retornar el estado inicial", async () => {
+    const { result, waitForNextUpdate } = renderHook(() => useFetchGifs("bbt"));
     const { data, loading } = result.current;
+    await waitForNextUpdate();
     expect(data).toEqual([]);
     expect(loading).toBe(true);
+  });
+
+  test("debe de retornar un arreglo de imagenes y el loading en false", async () => {
+    const { result, waitForNextUpdate } = renderHook(() => useFetchGifs("bbt"));
+    await waitForNextUpdate();
+    const { data, loading } = result.current;
+    expect(data.length).toBe(10);
+    expect(loading).toBe(false);
   });
 });
